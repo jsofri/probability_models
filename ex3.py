dd4d010b74876a53486a596c1184a0ae9f0ba723
jsofri <ysofri@nvidia.com>
2024-02-23 12:36:21 +0200

    (Ruben) added plots + perplexity

--- ex3.py
+++ ex3.py
@@ -2,6 +2,8 @@ from collections import Counter, defaultdict
 from typing import Optional
 from lidstone import UnigramLidstoneModel
 from math import log, exp
+import matplotlib.pyplot as plt
+import pandas as pd
 
 EPSILON: float = 1e-6
 LIDSTONE_CONSTANT: float = 0.1
@@ -12,6 +14,7 @@ THRESHOLD: float = 10.0
 class EM:
     def __init__(self, articles: dict[int, list[str]],
                  num_of_clusters: int,
+                 num_of_words: int,
                  vocabulary: int,
                  epsilon: float = EPSILON,
                  k: int = K,
@@ -26,6 +29,7 @@ class EM:
         self.k: int = k
         self.lidstone_constant: float = lidstone_constant
         self.vocabulary: int = vocabulary
+        self.num_of_words: int = num_of_words
 
         self.p: list[Optional[UnigramLidstoneModel]] = [None] * self.num_of_clusters
         self.w_t: list[list[float]] = [[] for _ in range(self.num_of_clusters)]
@@ -71,7 +75,7 @@ class EM:
             self.p[i] = UnigramLidstoneModel(self.lidstone_constant, list_of_words, self.vocabulary)
             list_of_words.clear()
 
-    def e_step(self):
+    def e_step(self) -> None:
         z: list[float] = [0.0] * self.num_of_clusters
 
         new_clusters: list[list[int]] = [[] for _ in range(self.num_of_clusters)]
@@ -98,7 +102,7 @@ class EM:
                 if z[i] - m < -self.k:
                     w_t[i] = 0.0
                 else:
-                    w_t[i] = exp_z[i] / sum([exp_z[j] for j in range(self.num_of_clusters) if z[j] - m >= -self.k])
+                    w_t[i] = exp_z[i] / sum([exp_z[j] for j in  range(self.num_of_clusters) if z[j] - m >= -self.k])
 
             new_clusters[w_t.index(max(w_t))].append(article)
             all_w_t.append(w_t)
@@ -110,18 +114,21 @@ class EM:
         self.m = all_m
         self.exp_z = all_exp_z
 
-    def m_step(self):
+    def m_step(self) -> None:
         self.compute_alpha()
         self.normalize_alpha()
         self.compute_p_ik()
 
-    def likelihood_calc(self):
+    def likelihood_calc(self) -> None:
         self.likelihood: float = 0.0
         for i in range(self.num_of_articles):
             self.likelihood += self.m[i] + log(sum([e for e in self.exp_z[i] if e - self.m[i] >= -self.k]))
 
+    def perplexity(self) -> float:
+        return exp((-self.likelihood / self.num_of_words))
 
-def main():
+
+def main() -> None:
     # parsing file
 
     article_to_words: dict[int, list[str]] = dict()
@@ -153,24 +160,67 @@ def main():
 
     # EM clustering
 
-    em = EM(article_to_words, 9, vocabulary_s)
+    em = EM(article_to_words, 9, len(all_words), vocabulary_s)
     em.initialization()
 
     last_likelihood: float = float('-inf')
     all_likelihoods: list[float] = []
+    all_perplexities: list[float] = []
+    iteration: int = 0
 
     while True:
         em.e_step()
         em.m_step()
         em.likelihood_calc()
 
+        iteration += 1
+
         all_likelihoods.append(em.likelihood)
-        print(f'(Log)-Likelihood: {all_likelihoods[-1]}')
+        all_perplexities.append(em.perplexity())
+
+        print(f'Log-Likelihood: {all_likelihoods[-1]}')
 
         if all_likelihoods[-1] - last_likelihood < THRESHOLD:
             break
 
         last_likelihood = all_likelihoods[-1]
+
+    # create plots
+
+    # log-likelihood plot
+    plt.plot(range(1, iteration + 1), all_likelihoods)
+    plt.xlabel('Iteration')
+    plt.ylabel('Log-Likelihood')
+    plt.title('Log-Likelihood vs. Iteration')
+    plt.show()
+
+    # perplexity plot
+    plt.plot(range(1, iteration + 1), all_perplexities)
+    plt.xlabel('Iteration')
+    plt.ylabel('Perplexity')
+    plt.title('Perplexity vs. Iteration')
+    plt.show()
+
+    # confusion matrix
+    # TODO
+
+    topics: list[str] = []
+
+    with open('topics.txt', 'r') as file:
+        for topic in file:
+            topics.append(topic.strip())
+
+    clusters_id_sorted: list[int] = sorted(range(len(em.clusters)), key=lambda x: len(em.clusters[x]), reverse=True)
+    rows: list[int] = [x + 1 for x in clusters_id_sorted]
+
+    matrix_data: list[list[int]] = [[0] * len(topics) for _ in range(len(em.clusters))]
+
+    for i in range(len(matrix_data)):
+        for j in range(len(matrix_data[i])):
+            # matrix_data[i][j] =
+            ...
+
+    matrix_df = pd.DataFrame(matrix_data, index=rows, columns=topics)
 
 
 if __name__ == '__main__':
