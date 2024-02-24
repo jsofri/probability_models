c5a769727ae008a3edf2431f9cab3c9af71ba1f5
jsofri <ysofri@nvidia.com>
2024-02-24 16:31:43 +0200

    added metrics and plots. acc=0.675

--- ex3.py
+++ ex3.py
@@ -4,11 +4,13 @@ from lidstone import UnigramLidstoneModel
 from math import log, exp
 import matplotlib.pyplot as plt
 import pandas as pd
+import re
 
 EPSILON: float = 1e-6
-LIDSTONE_CONSTANT: float = 0.1
+LIDSTONE_CONSTANT: float = 0.12
 K: int = 10
 THRESHOLD: float = 10.0
+SECTION_NUM_LINES: int = 4
 
 
 class EM:
@@ -128,24 +130,68 @@ class EM:
         return exp((-self.likelihood / self.num_of_words))
 
 
-def main() -> None:
+def to_png_file_name(y_label, plot_type):
+    file_name = y_label.lower().replace(" ", "_")
+    return f"{file_name}_{plot_type}.png"
+
+def save_plot(values, y_label):
+    plt.figure(figsize=(10, 5))
+    plt.plot(values)
+    plt.xlabel('Iterations')
+    plt.ylabel(y_label)
+    plt.title(f"{y_label} vs Iteration")
+    file_name = to_png_file_name(y_label, "plot")
+    plt.savefig(file_name)
+    print(f"{y_label} plot image: {file_name}")
+    plt.close()
+
+def save_histograms(topics, matrix_data, clusters_id_sorted, predictions):
+    fig, axs = plt.subplots(5, 2, figsize=(20, 20))
+    for i, ax in enumerate(axs.flat):
+        if i >= 9:
+            ax.axis('off')
+            continue
+        cid = clusters_id_sorted[i]
+        values = matrix_data[i][:-1]
+        ax.bar(topics, values)
+        ax.set_xticks(topics)  # redundant but deprecates warnings
+        ax.set_xticklabels(topics, fontweight='bold', fontsize='large')
+
+        ax.set_title(f'Cluster {cid + 1} ({predictions[cid]})', fontweight='bold', fontsize='xx-large')
+
+    plt.tight_layout()
+    image_name = to_png_file_name("clusters", "histogram")
+    fig.savefig(image_name, dpi=300)  # higher resolution
+    print(f"Saved histogram image: {image_name}")
+    plt.close(fig)
+
+def main():
     # parsing file
 
     article_to_words: dict[int, list[str]] = dict()
+    article_to_topics: dict[int, list[str]] = dict()
     all_words: list[str] = []
 
+    header_re = re.compile(r"<TRAIN\s+\d+\s+((\S+\s*)+)>\s*")
     with open('develop.txt', 'r') as file:
-        next(file)
-        next(file)
+        for i, line in enumerate(file):
+            article = i // SECTION_NUM_LINES
+            mod = i % SECTION_NUM_LINES
+            if mod == 0:
+                # article header line
+                match: re.Match = header_re.match(line)
+                if not match:
+                    print("line #{} expected to be a header but not follow header format")
+                    return 1
+                g1: str = match.group(1)
+                topics: list[str] = g1.split()
+                article_to_topics[article]= topics
+            elif mod == 2:
+                # article content line
+                words: list[str] = line.split(' ')[:-1]
 
-        for i, article in enumerate(file):
-            if i % 4 != 0:
-                continue
-
-            words: list[str] = article.split(' ')[:-1]
-
-            article_to_words[i // 4] = words
-            all_words += words
+                article_to_words[article] = words
+                all_words += words
 
     # filter rare words
 
@@ -154,12 +200,12 @@ def main() -> None:
     all_words = list(filter(lambda x: words_count[x] > 3, all_words))
     vocabulary_s: int = len(set(all_words))
     set_all_words = set(all_words)
+    print(f"voabulary size: {len(set_all_words)}")
 
     for i in range(len(article_to_words)):
         article_to_words[i] = list(filter(lambda x: x in set_all_words, article_to_words[i]))
 
     # EM clustering
-
     em = EM(article_to_words, 9, len(all_words), vocabulary_s)
     em.initialization()
 
@@ -178,50 +224,84 @@ def main() -> None:
         all_likelihoods.append(em.likelihood)
         all_perplexities.append(em.perplexity())
 
-        print(f'Log-Likelihood: {all_likelihoods[-1]}')
+        print(f'{iteration}) (Log)-Likelihood: {all_likelihoods[-1]:.3f}, Perplexity: {all_perplexities[-1]:.3f}')
 
         if all_likelihoods[-1] - last_likelihood < THRESHOLD:
             break
-
+        
         last_likelihood = all_likelihoods[-1]
 
     # create plots
 
-    # log-likelihood plot
-    plt.plot(range(1, iteration + 1), all_likelihoods)
-    plt.xlabel('Iteration')
-    plt.ylabel('Log-Likelihood')
-    plt.title('Log-Likelihood vs. Iteration')
-    plt.show()
+    save_plot(all_likelihoods, "Log Likelihood")
+    save_plot(all_perplexities, "Perplexity")
 
-    # perplexity plot
-    plt.plot(range(1, iteration + 1), all_perplexities)
-    plt.xlabel('Iteration')
-    plt.ylabel('Perplexity')
-    plt.title('Perplexity vs. Iteration')
-    plt.show()
+    # parse topics file
 
-    # confusion matrix
-    # TODO
+    topics = []
+    with open("topics.txt", "r") as fp:
+        for line in fp:
+            line = line.strip()
+            if line:
+                topics.append(line)
+    topic_to_column: dict[str, int] = {topic: idx for idx, topic in enumerate(topics)}
 
-    topics: list[str] = []
+    # set confusion matrix
 
-    with open('topics.txt', 'r') as file:
-        for topic in file:
-            topics.append(topic.strip())
+    matrix_data = []
+    n_rows = len(em.clusters)
+    n_cols = len(em.clusters) + 1  # +1 for cluster size 
+    for i in range(n_rows):
+        matrix_data.append([0] * n_cols)
 
     clusters_id_sorted: list[int] = sorted(range(len(em.clusters)), key=lambda x: len(em.clusters[x]), reverse=True)
+    cluster_id_to_row: dict[int, int] = {cid: idx for idx, cid in enumerate(clusters_id_sorted)}
+    article_to_cluster: dict[int, int] = {}
+    for cid, cluster in enumerate(em.clusters):
+        for article in cluster:
+            article_to_cluster[article] = cid
+    for article, atopics in article_to_topics.items():
+        cid = article_to_cluster[article]
+        row = cluster_id_to_row[cid]
+        for topic in atopics:
+            col = topic_to_column[topic]
+            matrix_data[row][col] += 1
+
+    # set predictions based on most common topic
+
+    predictions: dict[int, str] = {}
+    for i, cid in enumerate(clusters_id_sorted):
+        counts = matrix_data[i][:-1]
+        max_pos = counts.index(max(counts))
+        predictions[cid] = topics[max_pos]
+
+    # plot histograms based on confusion matrix
+
+    save_histograms(topics, matrix_data, clusters_id_sorted, predictions)
+
+    # add cluster size to matrix
+    for cid, row in cluster_id_to_row.items():
+        matrix_data[row][-1] = len(em.clusters[cid])
+
+    # log confusion matrix
+
     rows: list[int] = [x + 1 for x in clusters_id_sorted]
+    columns = [*topics, "cluster size"]
+    matrix_df = pd.DataFrame(matrix_data, index=rows, columns=columns)
+    print(matrix_df)
 
-    matrix_data: list[list[int]] = [[0] * len(topics) for _ in range(len(em.clusters))]
-
-    for i in range(len(matrix_data)):
-        for j in range(len(matrix_data[i])):
-            # matrix_data[i][j] =
-            ...
-
-    matrix_df = pd.DataFrame(matrix_data, index=rows, columns=topics)
-
+    # calculate model's accuracy
+    correct_assignments: int = 0
+    total_assignments: int = 0
+    for cid, cluster in enumerate(em.clusters):
+        for article in cluster:
+            real_topics = article_to_topics[article]
+            if predictions[cid] in real_topics:
+                correct_assignments += 1
+            total_assignments += 1
+    if total_assignments:
+        print(f"Model's accuracy: {correct_assignments/total_assignments:.3f}")
 
 if __name__ == '__main__':
-    main()
+    import sys
+    sys.exit(main())
